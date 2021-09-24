dc2d0d3e754d60be54911f6fa538f4631cacee80
Mao <doryantilmant25@gmail.com>
2021-09-24 16:06:40 +0200

    Build pour Github Pages (fix)

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,46 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Accueil from '@/views/Accueil.vue'
-import Inventory from '@/views/Inventory.vue';
-import Settings from '@/views/Settings.vue';
-import Summary from '@/views/Summary.vue';
-import Produit from '@/views/Produit.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import Accueil from "@/views/Accueil.vue";
+import Inventory from "@/views/Inventory.vue";
+import Settings from "@/views/Settings.vue";
+import Summary from "@/views/Summary.vue";
+import Produit from "@/views/Produit.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Accueil',
-    component: Accueil
+    path: "/",
+    name: "Accueil",
+    component: Accueil,
   },
   {
-    path: '/inventory',
-    name: 'Inventory',
-    component: Inventory
+    path: "/inventory",
+    name: "Inventory",
+    component: Inventory,
   },
   {
-    path: '/settings',
-    name: 'Settings',
-    component: Settings
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
   },
   {
-    path: '/summary',
-    name: 'Summary',
-    component: Summary
+    path: "/summary",
+    name: "Summary",
+    component: Summary,
   },
   {
-    path: '/produit/:slug',
-    name: 'Produit',
-    component: Produit
+    path: "/produit/:slug",
+    name: "Produit",
+    component: Produit,
   },
-]
+  {
+    path: "*",
+    name: "catchAll",
+    component: Accueil,
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
